ef4713e51ebe059092125412d3872a44903ccc5d
Luan Simões <luanc179@gmail.com>
2023-05-08 21:54:37 -0300

    ultimo do dia

--- aula20/DB.php
+++ aula20/DB.php
@@ -4,10 +4,10 @@ class DB{
     public static function getConnection()
     {
         if(isset(DB::$connection)){
-            return DB::$connection;
-        }else{
-            DB::$connection = new PDO('mysql:host=database:dsname=docker:port=3306','docker','docker');
+            DB::$connection = new PDO('mysql:host=database;dsname=docker;port=3308','docker','docker');
         }
+
+        return DB::$connection;
     }
 }
 
--- aula20/index.php
+++ aula20/index.php
@@ -1,7 +1,38 @@
 <?php
 require_once("DB.php");
 
-var_dump(DB::getConnection());
+$db = DB::getConnection();
 
+if(isset($_POST['acao']) &&($_POST['acao'] == 'Cadastrar')):
+    $nome = $_POST['nome']??'';
+    $sql= "INSERT INTO docker.aluno
+    (nome)
+    VALUES('$nome');
+    ";
+    if($db-exec($sql)==1)
+    echo "<p>Registro inserido!</p>";
+endif;
+
+?>
+
+
+<form action="" method="post">
+    <label for="nome">Nome:</label>
+    <input type="text" name="nome" id="nome">
+    <input type="submit" name="Cadastrar" id="acao">
+
+<?php
+$sql = 'SELECT id. nome FROM docker.aluno;';
+
+$alunos = $db-query($sql);
+
+echo "<h1> Toral de alunos: {$alunos->rowCount()}<h1>";
+
+echo "<ul>";
+
+    while($aluno = $alunos-fetch()){
+        echo "<li>{{$aluno['id']}: {$aluno['nome']} <li>\n";
+    }
+echo "<ul>";
 
 ?>
