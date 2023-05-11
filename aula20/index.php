c46484e320f694c7b8bb67a4991f430f450f0ae6
Luan Simões <luanc179@gmail.com>
2023-05-10 21:14:49 -0300

    aula 10-05

--- aula20/index.php
+++ aula20/index.php
@@ -3,15 +3,24 @@ require_once("DB.php");
 
 $db = DB::getConnection();
 
-if(isset($_POST['acao']) &&($_POST['acao'] == 'Cadastrar')):
+if(isset($_POST['acao']) &&($_POST['acao'] == 'Cadastrar')){
     $nome = $_POST['nome']??'';
     $sql= "INSERT INTO docker.aluno
     (nome)
     VALUES('$nome');
     ";
-    if($db-exec($sql)==1)
+
+    if($db-exec($sql)==1){
     echo "<p>Registro inserido!</p>";
-endif;
+}
+    elseif (isset($_GET['excluir'])){
+    $id = $_GET['excluir'];
+        $sql = "DELETE FROM docker.aluno
+        WHERE id=$id;";
+        if($db-exec($sql)==1)
+        echo "<p>Registro deletado!</p>";
+}
+
 
 ?>
 
@@ -31,7 +40,7 @@ echo "<h1> Toral de alunos: {$alunos->rowCount()}<h1>";
 echo "<ul>";
 
     while($aluno = $alunos-fetch()){
-        echo "<li>{{$aluno['id']}: {$aluno['nome']} <li>\n";
+        echo "<li>{{$aluno['id']}: {$aluno['nome']} <li> <a href=\"index.php?excluir={$aluno['id']}\">Deletar</a>\n";
     }
 echo "<ul>";
 
